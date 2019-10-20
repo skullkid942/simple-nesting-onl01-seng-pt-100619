3c0bd91bdd8b93a9313fd61b03b92f1965138e42
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-20 16:11:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto][:known_for]["Ruby"]
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages]["LISP","C"]
 end
 
